4cd9d6191822ca8cee45a584e6e3c3ead9039aaf
CastilloGit <jcarlos.castillov@gmail.com>
2020-12-08 23:35:38 +0100

    subida 117

--- preguntas/117/Program.cs
+++ preguntas/117/Program.cs
@@ -1,7 +1,11 @@
-﻿
+﻿/*QUESTION 117
+HOTSPOT
+You have the following code (line numbers are included for reference only):
+*/
+
 using System;
 using System.Data;
-using System.Data.SqlClient:
+using System.Data.SqlClient;
 
 namespace _117
 {
@@ -11,7 +15,7 @@ namespace _117
         {
             //Console.WriteLine("Hello World!");
             DataTable dataTable;
-            string connString = "Data Source=192.168.1.13;Initial Catalog=Database1;User Id = sa; Password=Zaq11qaz";
+            string connString = "Data Source=192.168.1.13;Initial Catalog=AdventureWorks2017;User Id =sa; Password=Zaq11qaz";
             using (SqlConnection sqlConn = new SqlConnection(connString))
             {
                 sqlConn.Open();
@@ -19,7 +23,7 @@ namespace _117
                 {
                     sqlCmd.Connection = sqlConn;
                     sqlCmd.CommandType = CommandType.StoredProcedure;
-                    sqlCmd.CommandText = "p_Procedure1";
+                    sqlCmd.CommandText = "uspGetBillOfMaterials";
                     using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCmd))
                     {
                         using (dataTable = new DataTable())
@@ -31,3 +35,22 @@ namespace _117
             }
         }
     }
+}
+/* 
+ * 
+Pregunta:
+the database connection gets closed at line 
+la conexión de la base de datos se cierra en la línea
+Respuesta: Linea 19 del texto original 35 del texto del presente còdigo
+
+the adapter object  get disposed at line 
+el objeto adaptador se desecha en la línea
+Respuesta: Linea 17 del texto de la pregunta 33 del texto del presente còdigo
+*
+
+Dificultad. Para que el código funcione he tenido que añadir el paquete nuget System.Data.SqlClient
+
+Right Click on Project > Manage Nuget Packages > Search & install 'System.Data.SqlClient'
+
+*/
+
