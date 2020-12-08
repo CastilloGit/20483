7ee7692943ac42b5f6eb2afd8d752eb73a3491da
CastilloGit <jcarlos.castillov@gmail.com>
2020-12-08 01:31:26 +0100

    subida117

--- preguntas/117/Program.cs
+++ preguntas/117/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using System;
+using System.Data;
+using System.Data.SqlClient:
 
 namespace _117
 {
@@ -6,7 +9,25 @@ namespace _117
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            DataTable dataTable;
+            string connString = "Data Source=192.168.1.13;Initial Catalog=Database1;User Id = sa; Password=Zaq11qaz";
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+            {
+                sqlConn.Open();
+                using (SqlCommand sqlCmd = new SqlCommand())
+                {
+                    sqlCmd.Connection = sqlConn;
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.CommandText = "p_Procedure1";
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCmd))
+                    {
+                        using (dataTable = new DataTable())
+                        {
+                            adapter.Fill(dataTable);
+                        }
+                    }
+                }
+            }
         }
     }
-}
